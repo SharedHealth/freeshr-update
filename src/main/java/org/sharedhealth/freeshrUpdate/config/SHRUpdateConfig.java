a72919d931fc88090b113136c35b80db2eafc927
rnjn <runjan@gmail.com>
2015-02-04 23:19:00 +0530

    Configuration <= local.properties

--- src/main/java/org/sharedhealth/freeshrUpdate/config/SHRUpdateConfig.java
+++ src/main/java/org/sharedhealth/freeshrUpdate/config/SHRUpdateConfig.java
@@ -6,11 +6,12 @@ import java.util.Map;
 
 @Component
 public class SHRUpdateConfig {
-    private String mciSchema;
+    private String mciScheme;
     private String mciHost;
     private String mciPort;
     private String mciUser;
     private String mciPassword;
+    private String mciContextPath;
     private String cassandraKeySpace;
     private String cassandraHost;
     private String cassandraPort;
@@ -18,7 +19,7 @@ public class SHRUpdateConfig {
     private String updateIntervalInSeconds;
 
     public String getMciSchema() {
-        return mciSchema;
+        return mciScheme;
     }
 
     public String getMciHost() {
@@ -57,8 +58,22 @@ public class SHRUpdateConfig {
         return Integer.parseInt(updateIntervalInSeconds);
     }
 
+    public String getMciBaseUrl() {
+        return mciScheme + "://" + mciHost + ":" + mciPort + "/" + mciContextPath;
+    }
+
     public SHRUpdateConfig() {
         Map<String, String> env = System.getenv();
         updateIntervalInSeconds = env.get("UPDATE_INTERVAL_SECONDS");
+        mciScheme = env.get("MCI_SCHEME");
+        mciHost = env.get("MCI_HOST");
+        mciPort = env.get("MCI_PORT");
+        mciContextPath = env.get("MCI_CONTEXT_PATH");
+        mciUser = env.get("MCI_USER");
+        mciPassword = env.get("MCI_PASSWORD");
+        cassandraHost = env.get("CASSANDRA_HOST");
+        cassandraPort = env.get("CASSANDRA_PORT");
+        cassandraKeySpace = env.get("CASSANDRA_KEYSPACE");
+        cassandraTimeout = env.get("CASSANDRA_TIMEOUT");
     }
 }
