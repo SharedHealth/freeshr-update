95fa05684bb55ad31ccc1678a8c994124f090cad
MD Dubey <mdinfotech007@gmail.com>
2015-03-26 10:59:45 +0530

    Dubey | bdhsr-659 | renamed log level property SHR_LOG_LEVEL to SHR_UPDATE_LOG_LEVEL in start.sh

--- scripts/rpm/start.sh
+++ scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java -DSHR_LOG_LEVEL=$SHR_LOG_LEVEL -jar /opt/freeshr-update/lib/freeshr-update.jar >  /dev/null 2>&1 &
+nohup java -DSHR_UPDATE_LOG_LEVEL=$SHR_UPDATE_LOG_LEVEL -jar /opt/freeshr-update/lib/freeshr-update.jar >  /dev/null 2>&1 &
 echo $! > /var/run/freeshr-update/freeshr-update.pid
